d0887a9c9b616ed521a3b80f5467edd551693d4a
Dan Zimmerman <daniel.zimmerman@me.com>
2014-01-31 22:39:43 -0600

    updated stylesheets, inverstigated having a default stylesheet, added css renderer, updated meta.json parsing

--- tests/app.js
+++ tests/app.js
@@ -1,25 +1,25 @@
 var express	= require("express"),
 	zdocs	= require("../lib/zdocs"),
-	stylus = require('stylus'),
-	nib = require('nib'),
+    stylus  = require('stylus'),
+    nib     = require('nib'),
 	app		= express();
 
 
-function compile(str, path) {
-	return stylus(str).set('filename', path).use(nib());
-}
+var compile = function(str, path) {
+    return stylus(str).set('filename', path).use(nib());
+};
 
-//app.use(express.logger('dev'));
 app.use(stylus.middleware({
-	src : __dirname + "/public",
-	compile : compile
+    src : __dirname + '/public',
+    compile : compile
 }));
 app.use(express.static(__dirname + '/public'));
 
+//app.use(express.logger('dev'));
 app.use('/', zdocs.middleware({
-	path : '../lib',
-	name : 'Test Documentation Center',
-	stylesheet : '/stylesheets/style.css'
+	paths : ['../lib', './public/stylesheets/'],
+	name : 'zDocs Center',
+    stylesheet : '/stylesheets/style.css'
 }));
 
 app.use(function(req, res) {
